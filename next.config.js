e10be05ba2e87b8f87e75b50748d8124ba3df170
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2022-02-20 11:07:30 +0530

    chore: init

---
+++ next.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const loaderUtils = require('loader-utils');
+
+const hashOnlyIdent = (context, _, exportName) =>
+  loaderUtils
+    .getHashDigest(
+      Buffer.from(
+        `filePath:${path
+          .relative(context.rootContext, context.resourcePath)
+          .replace(/\\+/g, '/')}#className:${exportName}`,
+      ),
+      'md4',
+      'base64',
+      6,
+    )
+    .replace(/^(-?\d|--)/, '_$1');
+
+module.exports = {
+  webpack(config, { dev }) {
+    const rules = config.module.rules
+      .find((rule) => typeof rule.oneOf === 'object')
+      .oneOf.filter((rule) => Array.isArray(rule.use));
+
+    if (!dev)
+      rules.forEach((rule) => {
+        rule.use.forEach((moduleLoader) => {
+          if (
+            moduleLoader.loader?.includes('css-loader') &&
+            !moduleLoader.loader?.includes('postcss-loader')
+          )
+            moduleLoader.options.modules.getLocalIdent = hashOnlyIdent;
+        });
+      });
+
+    return config;
+  },
+};
---
+++ pages/index.jsx
@@ -0,0 +1,5 @@
+import styles from '../styles/index.module.css';
+
+const Home = () => <div className={styles.foo}>Hello World</div>;
+
+export default Home;
