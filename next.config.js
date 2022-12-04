54089bc43782bc277343da5c6a6eea6a991516d6
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2022-12-04 19:53:49 +0530

    chore: init

---
+++ next.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const loaderUtils = require('loader-utils')
+
+const hashOnlyIdent = (context, _, exportName) =>
+  loaderUtils
+    .getHashDigest(
+      Buffer.from(
+        `filePath:${path
+          .relative(context.rootContext, context.resourcePath)
+          .replace(/\\+/g, '/')}#className:${exportName}`
+      ),
+      'md4',
+      'base64',
+      6
+    )
+    .replace(/[^a-zA-Z0-9-_]/g, '_')
+    .replace(/^(-?\d|--)/, '_$1')
+
+/**
+ * @type {import('next').NextConfig}
+ */
+module.exports = {
+  experimental: { appDir: true },
+
+  webpack(config, { dev }) {
+    const rules = config.module.rules
+      .find((rule) => typeof rule.oneOf === 'object')
+      .oneOf.filter((rule) => Array.isArray(rule.use))
+
+    if (!dev)
+      rules.forEach((rule) => {
+        rule.use.forEach((moduleLoader) => {
+          if (
+            moduleLoader.loader?.includes('css-loader') &&
+            !moduleLoader.loader?.includes('postcss-loader')
+          )
+            moduleLoader.options.modules.getLocalIdent = hashOnlyIdent
+        })
+      })
+
+    return config
+  }
+}
